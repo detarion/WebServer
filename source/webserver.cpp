67cd57a987166013bf8c2f97e6cd62db85a0a3d5
User <test@example.com>
2026-02-01 19:39:18 +0700

    Реализация через epoll

--- source/webserver.cpp
+++ source/webserver.cpp
@@ -4,14 +4,23 @@
 #include <csignal>
 #include <arpa/inet.h>
 #include <atomic>
+#include <sys/epoll.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <vector>
 
-// Глобальный флаг для обработки сигналов
 static std::atomic<bool> g_running{true};
 
-// Обработчик сигналов завершения
-void signalHandler(int signum)
+void signalHandler(int)
 {
     g_running = false;
+}
+
+// Утилита для перевода сокета в неблокирующий режим
+int set_nonblocking(int fd)
+{
+    int flags = fcntl(fd, F_GETFL, 0);
+    return fcntl(fd, F_SETFL, flags | O_NONBLOCK);
 }
 
 class SimpleWebServer
@@ -20,145 +29,109 @@ class SimpleWebServer
     char logBuffer_[100];
 
 public:
-    SimpleWebServer(const std::string& filename) : logfile_(filename.c_str(), std::ios::app)
+    SimpleWebServer(const std::string& filename) : logfile_(filename, std::ios::app)
     { }
 
     void process(uint16_t port, const std::string& content)
     {
-        signal(SIGTERM, signalHandler); // kill
+        signal(SIGINT, signalHandler);
+        signal(SIGTERM, signalHandler);
         signal(SIGPIPE, SIG_IGN);
 
-        log("ok - start");
-
-        auto server_fd = socket(AF_INET, SOCK_STREAM, 0);
+        int server_fd = socket(AF_INET, SOCK_STREAM, 0);
         if (server_fd < 0)
         {
             log(strerror(errno));
             return;
         }
-        
-        log("ok - socket");
 
-        int optVal{1};
-        if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &optVal, sizeof(optVal)) < 0)
-        {
-            log(strerror(errno));
-            close(server_fd);
-            return;
-        }
-        
-        log("ok - setsockopt");
+        int optVal = 1;
+        setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &optVal, sizeof(optVal));
+        set_nonblocking(server_fd);
 
-        struct timeval accept_tv = {1, 0};
-        if (setsockopt(server_fd, SOL_SOCKET, SO_RCVTIMEO, &accept_tv, sizeof(accept_tv)) < 0)
-        {
-            log(strerror(errno));
-            close(server_fd);
-            return;
-        }
-        
-        log("ok - setsockopt");
-
-        struct sockaddr_in address;
+        sockaddr_in address{};
         address.sin_family = AF_INET;
         address.sin_addr.s_addr = INADDR_ANY;
         address.sin_port = htons(port);
 
         if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0)
         {
-            log(strerror(errno));
-            close(server_fd);
+            log(strerror(errno)); close(server_fd);
             return;
         }
 
-        log("ok - bind");
+        listen(server_fd, SOMAXCONN);
 
-        if (listen(server_fd, 10) < 0)
-        {
-            log(strerror(errno));
-            close(server_fd);
-            return;
-        }
+        // Инициализация epoll
+        int epoll_fd = epoll_create1(0);
+        epoll_event event{}, events[64];
+        event.events = EPOLLIN; // Отслеживаем входящие соединения
+        event.data.fd = server_fd;
+        epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_fd, &event);
 
-        log("ok - listen");
+        log("Сервер epoll запущен на порту " + std::to_string(port));
 
-        log("Сервер успешно запущен на порту " + std::to_string(port));
-
-        socklen_t addrLen = sizeof(address);
-        char buffer[1024];
-
+        char buffer[1024]{};
+        
         while (g_running)
         {
-            int clientSocket = accept(server_fd, (struct sockaddr*)&address, &addrLen);
-            if (clientSocket > 0)
+            int nfds = epoll_wait(epoll_fd, events, 64, 500); // Таймаут 500мс
+            
+            for (int i = 0; i < nfds; ++i)
             {
-                struct timeval tv = {5, 0};
-                if (setsockopt(clientSocket, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) == 0)
+                if (events[i].data.fd == server_fd)
                 {
-                    auto readBytes = read(clientSocket, buffer, sizeof(buffer) - 1);
-                    if (readBytes > 0)
+                    // Новое соединение
+                    int client_fd = accept(server_fd, nullptr, nullptr);
+                    if (client_fd > 0)
                     {
-                        if (send(clientSocket, content.c_str(), content.size(), 0) > 0)
-                        {
-                            // success
-                        }
-                        else
-                            log(strerror(errno));
-                    }
-                    else
-                    {
-                        if (errno == EAGAIN || errno == EWOULDBLOCK)
-                            log("Клиент отключен по таймауту");
-                        else
-                            log(strerror(errno));
+                        set_nonblocking(client_fd);
+                        event.events = EPOLLIN | EPOLLET; // Edge-Triggered
+                        event.data.fd = client_fd;
+                        epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &event);
                     }
                 }
                 else
-                    log(strerror(errno));                            
-                
-                close(clientSocket);
-            }
-            else
-            {
-                if (errno != EAGAIN && errno != EWOULDBLOCK)
-                    log(strerror(errno));
+                {
+                    // Данные от клиента
+                    int client_fd = events[i].data.fd;
+                    int bytes = read(client_fd, buffer, sizeof(buffer));
+                    if (bytes > 0)
+                    {
+                        send(client_fd, content.c_str(), content.size(), 0);
+                    }
+
+                    // Закрываем и удаляем из epoll
+                    epoll_ctl(epoll_fd, EPOLL_CTL_DEL, client_fd, nullptr);
+                    close(client_fd);
+                }
             }
         }
-
-        log("Завершение работы сервера...");
+        
         close(server_fd);
+        close(epoll_fd);
+        log("Сервер остановлен.");
     }
 
     void log(const std::string_view message)
     {
-        if (logfile_.is_open())
-        {
-            auto time = std::chrono::system_clock::now();
-            auto time_t = std::chrono::system_clock::to_time_t(time);
-            
-            tm time_tm;
-            localtime_r(&time_t, &time_tm);
-    
-            const char format[] = "%Y-%m-%d %H:%M:%S";
-            strftime(logBuffer_, sizeof(logBuffer_), format, &time_tm);
-    
-            logfile_ << logBuffer_ << ": " << message << '\n';
-            logfile_.flush();
-        }
+        if (!logfile_.is_open())
+            return;
+
+        auto time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+        tm tm_time;
+        localtime_r(&time, &tm_time);
+        strftime(logBuffer_, sizeof(logBuffer_), "%Y-%m-%d %H:%M:%S", &tm_time);
+        logfile_ << logBuffer_ << ": " << message << std::endl;
     }
 };
 
 int main()
 {
-    std::string content = "<html><body><img src='https://fs.moex.com/f/11840/moex.jpg' width='600'/></body></html>";
-    
-    std::string response = "HTTP/1.1 200 OK\r\nContent-Length: "
-        + std::to_string(content.size())
-        + "\r\n\r\n"
-        + content;
+    std::string content = "<html><body><h3>Webserver: single-threaded, async epoll</h3><img src='https://fs.moex.com/f/11840/moex.jpg' width='600'/></body></html>";
+    std::string response = "HTTP/1.1 200 OK\r\nContent-Length: " + std::to_string(content.size()) + "\r\nConnection: close\r\n\r\n" + content;
 
     SimpleWebServer server("./webserver.log");
     server.process(8080, response);
-
     return 0;
 }
